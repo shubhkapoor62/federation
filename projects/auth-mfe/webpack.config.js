5ed4433a9ff38139255584ee1b4e791ff359c424
unknown <shubhkapoor62@gmail.com>
2024-01-18 11:50:57 +0530

    web config updated

---
+++ projects/auth-mfe/webpack.config.js
@@ -0,0 +1,17 @@
+const {
+  shareAll,
+  withModuleFederationPlugin
+} = require('@angular-architects/module-federation/webpack');
+module.exports = withModuleFederationPlugin({
+  name: 'auth-mfe',
+  exposes: {
+      './Module': './projects/auth-mfe/src/app/auth/auth.module.ts',
+  },
+  shared: {
+      ...shareAll({
+          singleton: true,
+          strictVersion: true,
+          requiredVersion: 'auto'
+      }),
+  },
+});
